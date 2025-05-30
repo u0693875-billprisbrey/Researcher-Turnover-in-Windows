9ad78c9cba472de628d94cb8884cd54e426f9b42
u0693875-billprisbrey <u0693875@umail.utah.edu>
2025-05-30 16:17:52 +0000

    Added notes from a coordination meeting with HR.
    Creating a powerpoint version for sharing graphics from the Retention Exploratory Data Analysis.
    
    Additional work on the plotMetrics multiple lines CHECK document.

--- Sandbox/plotMetrics multiple lines CHECK.R
+++ Sandbox/plotMetrics multiple lines CHECK.R
@@ -125,7 +125,7 @@ compareMetrics <- Map(function(x, y) merge(x, y, by = "adjDate", all = TRUE), co
 lapply(compareMetrics, function(x){
   
   filter <- x[,"adjDate"] %in% c(2013:2015)
-  x[filter,c("termRate.x","termRate.y")]
+  x[filter, c("termCount.x","termCount.y", "delta.x","delta.y", "headcount_mean.x", "headcount_mean.y")] # c("termCount.x","termCount.y")
   
 })
 
@@ -133,3 +133,326 @@ lapply(compareMetrics, function(x){
 # that's interesting.
 
 # I gotta get to the bottom of that.
+
+
+lapply(compareMetrics, function(x){
+  
+  filter <- x[,"adjDate"] %in% c(2013:2015)
+  apply(x[filter, c("headcount_mean.x", "headcount_mean.y")], 1, function(y){y[2]-y[1]}) # c("termCount.x","termCount.y")
+  
+})
+
+
+# so it looks like the mean is calculated differently in 2015
+
+# how much do I care?
+# let's try it again for months
+
+# it probably has to do with the starting date.
+
+# let's try this --
+
+
+complexMetrics31 <- lapply(complexPIs, function(x){
+  
+  calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                   minDate = ymd("2012-12-31"),
+                   maxDate = today(), 
+                   calendar = "year")
+  
+})
+
+
+lapply(complexMetrics, function(x) {x[,"headcount_mean"]})
+lapply(complexMetrics31, function(x) {x[,"headcount_mean"]})
+
+compareMetrics <- Map(function(x, y) merge(x, y, by = "adjDate", all = TRUE), complexMetrics, complexMetrics31)
+lapply(compareMetrics, function(x){
+  
+  filter <- x[,"adjDate"] %in% c(2013:2015)
+  apply(x[filter, c("headcount_mean.x", "headcount_mean.y")], 1, function(y){y[2]-y[1]}) # c("termCount.x","termCount.y")
+  
+})
+
+
+lapply(compareMetrics, function(x){
+  x[,c("headcount_mean.x","headcount_mean.y", "hireRate.x","hireRate.y","termRate.x", "termRate.y")]
+  
+})
+
+lapply(compareMetrics, function(x){
+  x[,c("adjDate", "headcount_mean.x","headcount_mean.y")]
+  
+})
+
+lapply(compareMetrics, function(x){
+  x[,c("adjDate", "hireRate.x","hireRate.y")]
+  
+})
+
+lapply(compareMetrics, function(x){
+  x[,c("adjDate", "termRate.x", "termRate.y")]
+  
+})
+
+# I think it's just a caution to be aware of start and end date effects
+
+# I could compare staring on May 13, for example, and probably see this disappear
+
+# And I could just look for whether I used > or >=
+
+# as it is I'll not worry about it.
+
+complexMetricsDay <- lapply(complexPIs, function(x){
+  
+  calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                   minDate = ymd("2014-12-25"),
+                   maxDate = ymd("2015-01-05"), 
+                   calendar = "day")
+  
+})
+
+lapply(complexMetricsDay, function(x){
+  x[,c("adjDate", "delta" )]
+  
+})
+
+
+# But wait --- did I really have zero terminations in 2013?
+
+
+calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                 minDate = ymd("2014-12-25"),
+                 maxDate = ymd("2015-01-05"), 
+                 calendar = "day") |>
+  plotMetrics()
+
+
+# I have 62 terminations in 2013 per earlySpan[["year"]]
+# if I sum the terminations for all clusters, do I get 62?
+
+# also, given:
+plotMetrics(earlySpan[["year"]], hire_points_params = list(type = "n"), metric_plot_params=list(ylim = c(0,5)))
+# Warning message:
+#  In (function (..., no.readonly = FALSE)  :
+#        "ylim" is not a graphical parameter
+
+
+# ylim is established based on the presence of hire points line.
+# even if I don't show it, the y axis range is way off
+# I'm not sure how much I care
+
+# I could fix it with an "if" condition when I set the ylim
+# another day, I think, as I have bigger fish to fry.
+
+
+lapply(complexMetrics, function(x){x[x$adjDate == 2013,"termCount"]})
+
+lapply(complexMetrics, function(x){x[,c("adjDate", "termCount")]})
+
+
+# OK, WE HAVE A DISCREPANCY!
+# I gotta figure this one out
+# ugh
+
+# I'm hoping it's just a Jan 1st thing.  Gee whiz.
+
+complexMetrics_Dec31 <- lapply(complexPIs, function(x){
+  
+  calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                   minDate = ymd("2012-12-31"),
+                   maxDate = today(), 
+                   calendar = "year")
+  
+})
+
+sapply(complexMetrics_Dec31, function(x){x[x$adjDate == 2013,"termCount"]}) |> sum() # 2
+
+lapply(complexMetrics_Dec31, function(x){x[,c("adjDate", "termCount")]})
+
+# Uh oh.
+
+# ok, let's get the PI's of all the folks terminated in 2013
+# and see what cluster they are in
+
+PI2013Term <- retData$PI_EMPLID[year(retData$TERMINATION_DT) == 2013 & !is.na(retData$TERMINATION_DT)] # pi's terminated in 2013
+
+length(PI2013Term) # 62 # good, check
+
+any(PI2013Term %in% piEmplid$emplid) # FALSE
+
+# whoa, really? !! Wow!
+
+# well o.k. then
+
+# I guess these aren't comparable due to different filters
+
+all(piEmplid$emplid %in% retData$PI_EMPLID) # TRUE (good)
+all(prepData$PROPOSAL_PI_EMPLID %in% retData$PI_EMPLID) # TRUE (good)
+
+table(retData$PI_EMPLID %in% prepData$PROPOSAL_PI_EMPLID)
+FALSE  TRUE 
+1538  2937 
+
+# This is a good thing to describe in the report
+# Presumably these are people who published BEFORE 2013 (my cut-off date)
+
+plotMetrics(complexMetrics, plotList = "metric", hire_points_params = list(type = "n"))
+
+# Why isn't this working?
+# Oh.  New error message added
+
+plotMetrics(complexMetrics, plotList = "rate", 
+            metric_plot_params = list(oma = c(0,0,2,0)),
+            hire_points_params = list(type = "n"),
+            featureMap = complexClusterColors)
+
+plotMetrics(complexMetrics, plotList = c("rate", "frank","beans"), 
+        #    metric_plot_params = list(oma = c(0,0,2,0)),
+            hire_points_params = list(type = "n"),
+            featureMap = complexClusterColors)
+
+
+# let's look at complexMetrics (hiring and firing) over the entire life
+
+
+complexMetrics_fullspan <- lapply(complexPIs, function(x){
+  
+  calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                   minDate = ymd("1960-01-01"),
+                   maxDate = today(), 
+                   calendar = "year")
+  
+})
+
+plotMetrics(complexMetrics_fullspan,
+            plotList = "all",
+            featureMap = complexClusterColors)
+
+# looks like it can't handle a 0/0, or rate values of NaN
+
+# Is this really the first time that's come up?
+
+# Yes, because a "headcount_mean" value of zero or near-zero is useless.
+# I need to tighten my time frame to give that some meaning.
+
+plotMetrics(complexMetrics_fullspan[[1]],
+            plotList = "all",
+            featureMap = complexClusterColors
+            )
+
+plotMetrics(complexMetrics_fullspan[[1]],
+            plotList = "count",
+            metric_legend2_params = list(plot = FALSE),
+            featureMap = complexClusterColors
+)
+
+
+
+complexMetrics_2000 <- lapply(complexPIs, function(x){
+  
+  calculateMetrics(data=retData[retData$PI_EMPLID %in% x,],
+                   minDate = ymd("2000-01-01"),
+                   maxDate = today(), 
+                   calendar = "year")
+  
+})
+
+plotMetrics(complexMetrics_2000,
+            plotList = "all",
+            featureMap = complexClusterColors
+)
+
+# ok, interesting.
+# do I care?
+
+plotMetrics(complexMetrics_2000,
+            plotList = c("rate"),
+            
+            featureMap = complexClusterColors
+)
+
+# this looks like it explains how the data was collectd,
+# or the filters that define this data set, than anything else.
+
+# Rather than plot a line-per-cluster, here's some ideas:
+# double-check that everyone in prepData is assigned a cluster value (including "unassigned")
+# create a single line that only includes this population
+
+# so I have a single line that does the full population in the view
+# then I do another sinlge line that does the population in the view that is found in prepData
+
+
+
+calculateMetrics(data=retData[retData$PI_EMPLID %in% prepData$PROPOSAL_PI_EMPLID,],
+                 minDate = ymd("2000-01-01"),
+                 maxDate = today(), 
+                 calendar = "year") |>
+  plotMetrics()
+
+
+calculateMetrics(data=retData,
+                 minDate = ymd("2000-01-01"),
+                 maxDate = today(), 
+                 calendar = "year") |>
+  plotMetrics()
+
+# both of these have a termination rate of essentially zero
+# until it turns on at one point.
+
+# I'd like to prosaically explain that.
+
+# is it because we aren't pulling in the old-timers that are retiring
+# during a given period, but selecting the people who are active later,
+# therefore hired at this earlier time (high hire rate)
+# ..and then are terminated?
+
+
+# I think I'm looking at a definitional problem.  It's not a good cross-section.
+
+# Except it's fine if I can keep the denominator correct
+# if I can define "this" group of people out of "that"
+
+# As in, my data should be all of retData, and my subset is the population
+# of a cluster.
+
+# This will take some thinking.  I might have my denominator that defines my rates
+# mis-calculated.
+
+compList <- list(complete = calculateMetrics(data=retData,
+                             minDate = ymd("2010-01-01"),
+                             maxDate = today(), 
+                             calendar = "year"),
+    active = calculateMetrics(data=retData[retData$PI_EMPLID %in% prepData$PROPOSAL_PI_EMPLID,],
+                         minDate = ymd("2010-01-01"),
+                         maxDate = today(), 
+                         calendar = "year"),
+    inactive = calculateMetrics(data=retData[!retData$PI_EMPLID %in% prepData$PROPOSAL_PI_EMPLID,],
+                                minDate = ymd("2010-01-01"),
+                                maxDate = today(), 
+                                calendar = "year")
+     )
+
+plotMetrics(compList)
+
+# some fascinating things happening here.
+
+# I guess I have this full data set, with some unknown definitions defining the population,
+# and it makes sense that 
+# that the termination rate would start out at near zero if I am defining the population
+# as being active as of some date.  They would be active headcount on Day 1 of the period,
+# and wouldn't have had time to quit yet.
+# Similarly, "hiring" rate would start out really high as I grew the population.
+# But neither is a very accurate snapshot of the actual active population.
+
+# I've got some interesting population dynamics to consider here.
+
+# And, as far as improving the graphics,
+# I can define things like yLim -after- modifying the list of parameters
+# that would make the plot more accurate to what I am trying to represent.
+
+
+
+
+
+
